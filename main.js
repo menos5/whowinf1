e0e4610e266cd16ed07a14e3a288ed643416a0fb
Tomas <menos5@gmail.com>
2015-03-16 16:43:12 +0100

    constructors

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 angular.module("myapp", [])
-.controller("MyController", function($scope, $http) {
+.controller("Drivers", function($scope, $http) {
   $scope.myData =  $http.get("http://ergast.com/api/f1/current/driverStandings.json").
     success(function(data, status, headers, config) {
 
@@ -27,6 +27,43 @@ angular.module("myapp", [])
 
       $scope.c = function() {
         var d =  ($scope.carreras * 25 / 100);
+        return d;
+      }
+
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+
+} )
+.controller("Constructors", function($scope, $http) {
+  $scope.myData =  $http.get("http://ergast.com/api/f1/current/constructorStandings.json").
+    success(function(data, status, headers, config) {
+      $scope.resultados = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+      $scope.round = data.MRData.StandingsTable.StandingsLists[0].round;
+      $scope.carreras = 19;
+      $scope.primero = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0].points;
+
+      $scope.prob = function(puntos) {
+        if ( (($scope.carreras - $scope.round) * 43) < ($scope.primero - puntos) ) {
+          return "eliminado";
+        }
+      };
+
+      $scope.a = function(puntos) {
+        var d = (puntos / $scope.c());
+        return d;
+      }
+
+      $scope.b = function(puntos) {
+        var posibles = ($scope.carreras - $scope.round) * 43;
+        var d =  (parseInt(puntos) + posibles ) / $scope.c();
+        return d;
+      }
+
+      $scope.c = function() {
+        var d =  ($scope.carreras * 43 / 100);
         return d;
       }
 
